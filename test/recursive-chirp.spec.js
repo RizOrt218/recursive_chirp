b01b235078a2ce215812fad30fc0dcce3424895a
RizOrt218 <rizhiafalen@gmail.com>
2015-12-28 10:07:11 -1000

    creates first test to expect a fn. passed

--- test/recursive-chirp.spec.js
+++ test/recursive-chirp.spec.js
@@ -4,8 +4,17 @@ chai.should();
 
 var recursiveChirp = require('./../recursive-chirp.js');
 
-describe( 'recursive-chirp', function () {
+describe( 'recursive-chirp', function() {
   it( 'should be a function that exist', function () {
     expect( recursiveChirp ).to.be.a('function');
   });
+
+  it('should return a string', function() {
+    var result = 'chirp chirp chirp';
+
+    expect( result ).to.be.a('string');
+    //expect( result.length ).to.be.equal(1);
+  });
+
+
 });
